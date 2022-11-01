d473664a3fe30eee4570b35a226e429deb096338
Michal Nowikowski <godfryd@gmail.com>
2022-11-01 20:24:31 +0100

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# poetry-arbitrary-version-plugin
-A Poetry plugin to override a version in a pyproject.toml from environment variable or build command option
+# Poetry Arbitrary Version Plugin
+
+[![CI](https://github.com/godfryd/poetry-arbitrary-version-plugin/actions/workflows/build.yml/badge.svg)](https://github.com/godfryd/poetry-arbitrary-version-plugin/actions/workflows/build.yml)
+
+A [Poetry](https://python-poetry.org/) plugin that allows a project
+builder to override the project version.  A project version can be
+overriden for example during CI process. The version can be overriden
+using the environment variable `PROJECT_OVERRIDE_VERSION` or
+the `--override-version` switch of the build command.
+
+## Usage
+
+Overriding a project version by `PROJECT_OVERRIDE_VERSION` environment variable:
+
+```console
+$ PROJECT_OVERRIDE_VERSION=3.2.1 poetry build -f sdist
+Overriden project version from 0.8.0 to 3.2.1
+Building poetry-arbitrary-version-plugin (3.2.1)
+  - Building sdist
+  - Built poetry_arbitrary_version_plugin-3.2.1.tar.gz
+```
+
+Overriding a project version by the `--override-version` switch:
+
+```console
+$ poetry build -f sdist --override-version=1.2.3
+Overriden project version from 0.8.0 to 1.2.3
+Building poetry-arbitrary-version-plugin (1.2.3)
+  - Building sdist
+  - Built poetry_arbitrary_version_plugin-1.2.3.tar.gz
+```
+
+## Install
+
+```sh
+poetry self add poetry-arbitrary-version-plugin
+```
---
+++ poetry_arbitrary_version_plugin/plugin.py
@@ -0,0 +1,44 @@
+import os
+
+from cleo.events.console_events import COMMAND
+from cleo.helpers import option
+from poetry.console.application import Application
+from poetry.console.commands.env_command import EnvCommand
+from poetry.console.commands.build import BuildCommand
+from poetry.plugins.application_plugin import ApplicationPlugin
+
+
+def set_new_version(app, new_version, io):
+    old_version = app.poetry.package.version.text
+    app.poetry.package.version = new_version
+    io.write_line("Overriden project version from %s to %s" % (old_version, new_version))
+
+
+def my_build_handle(self):
+    # check if --override-version is used, if so then override project version
+    new_version = self.option('override-version')
+    if new_version:
+        set_new_version(self.application, new_version, self.io)
+    else:
+        # if --override-version is not used
+        # then check if PROJECT_OVERRIDE_VERSION environment variable is used
+        # if so then override project version
+        new_version = os.environ.get('PROJECT_OVERRIDE_VERSION', None)
+        if new_version:
+            set_new_version(self.application, new_version, self.io)
+
+    # run original build handle method
+    self.handle_orig()
+
+
+class ArbitraryVersionPlugin(ApplicationPlugin):
+    def activate(self, application):
+        # add new --override-version option to build command
+        BuildCommand.options.append(option("override-version",
+                                           description="Override project version defined in pyproject.toml with your arbitrary version.",
+                                           flag=False))
+
+        # hijack build handle method to capture new --override-version option
+        # and if used then override project version
+        BuildCommand.handle_orig = BuildCommand.handle
+        BuildCommand.handle = my_build_handle
