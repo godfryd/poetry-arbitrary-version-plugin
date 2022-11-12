96b344dde05c80ab316eebbddbb842a4873ff86c
Michal Nowikowski <godfryd@gmail.com>
2022-11-12 12:33:30 +0100

    fixed storing overriden version in metadata of sdist pkg

--- poetry_arbitrary_version_plugin/plugin.py
+++ poetry_arbitrary_version_plugin/plugin.py
@@ -1,4 +1,7 @@
 import os
+import re
+import pathlib
+import tempfile
 
 from cleo.helpers import option
 from poetry.console.application import Application
@@ -6,11 +9,19 @@ from poetry.console.commands.build import BuildCommand
 from poetry.console.commands.publish import PublishCommand
 from poetry.plugins.application_plugin import ApplicationPlugin
 
+from poetry.core.masonry.builders.sdist import SdistBuilder
+from poetry.core.masonry.builders.builder import BuildIncludeFile
+
+
+MY_VERSION = None
+
 
 def set_new_version(app, new_version, io):
+    global MY_VERSION
     old_version = app.poetry.package.version.text
     app.poetry.package.version = new_version
     io.write_line("Overriden project version from %s to %s" % (old_version, new_version))
+    MY_VERSION = new_version
 
 
 def my_handle(self):
@@ -30,6 +41,27 @@ def my_handle(self):
     self.handle_orig()
 
 
+class MyBuildIncludeFile(BuildIncludeFile):
+    def relative_to_source_root(self):
+        return pathlib.Path('pyproject.tml')
+
+
+def my_find_files_to_add(self, exclude_build=False):
+    files = self.find_files_to_add_orig(exclude_build)
+    files2 = []
+    for f in files:
+        if f.path.name == 'pyproject.toml':
+            proj_txt = f.path.read_text()
+            ver_line = 'version="%s"' % MY_VERSION
+            proj_txt_patched = re.sub('^version.*=.*$', ver_line, proj_txt, flags=re.M)
+            fp = tempfile.NamedTemporaryFile("w", delete=False)
+            fp.write(proj_txt_patched)
+            fp.flush()
+            f = MyBuildIncludeFile(path=fp.name, project_root=self._path, source_root=self._path)
+        files2.append(f)
+    return files2
+
+
 class ArbitraryVersionPlugin(ApplicationPlugin):
     def activate(self, application):
         for cmd_cls in [BuildCommand, PublishCommand]:
@@ -42,3 +74,6 @@ class ArbitraryVersionPlugin(ApplicationPlugin):
             # and if used then override project version
             cmd_cls.handle_orig = cmd_cls.handle
             cmd_cls.handle = my_handle
+
+        SdistBuilder.find_files_to_add_orig = SdistBuilder.find_files_to_add
+        SdistBuilder.find_files_to_add = my_find_files_to_add
